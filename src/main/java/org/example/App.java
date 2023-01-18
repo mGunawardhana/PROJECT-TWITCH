1c980d64538359c390b1ecf5915383855083cde4
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-18 13:05:17 +0530

    introducing createDocuments method

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,12 +2,9 @@ package org.example;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoIterable;
 import org.bson.Document;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -24,17 +21,19 @@ public class App {
         String cnnectionString = "mongodb+srv://twitch:dinithi12345@twitchcluster.yqnkxgo.mongodb.net/?retryWrites=true&w=majority";
         try (MongoClient mongoClient = MongoClients.create(cnnectionString)) {
             Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+            /*printDatabases(mongoClient);*/
 
-            ArrayList<Document> documents = mongoClient.listDatabases().into(new ArrayList<>());
-            documents.forEach(document -> System.out.println(document.toJson()));
-
-
-//            MongoIterable<String> strings = mongoClient.listDatabaseNames();
-//            MongoCursor<String> cursor = strings.cursor();
-//            while (cursor.hasNext()){
-//                System.out.println(cursor.hasNext());
-//            }
+            createDocuments(mongoClient);
         }
+    }
+
+    private static void createDocuments(MongoClient mongoClient) {
+
+    }
+
+    private static void printDatabases(MongoClient mongoClient) {
+        ArrayList<Document> documents = mongoClient.listDatabases().into(new ArrayList<>());
+        documents.forEach(document -> System.out.println(document.toJson()));
     }
 }
 
