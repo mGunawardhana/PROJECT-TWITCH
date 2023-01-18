bdbd5fb89ef275fc56cdf6478bcaa2af4713c2a7
Maneesha Gunawardhana <100486080+mGunawardhana@users.noreply.github.com>
2023-01-18 12:57:40 +0530

    getting databases on Atlas

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,10 +1,41 @@
 package org.example;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoIterable;
+import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        /* removing loggers and get only warnings */
+        Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+
+
+        String cnnectionString = "mongodb+srv://twitch:dinithi12345@twitchcluster.yqnkxgo.mongodb.net/?retryWrites=true&w=majority";
+        try (MongoClient mongoClient = MongoClients.create(cnnectionString)) {
+            Logger.getLogger("org.mongodb.driver").setLevel(Level.WARNING);
+
+            ArrayList<Document> documents = mongoClient.listDatabases().into(new ArrayList<>());
+            documents.forEach(document -> System.out.println(document.toJson()));
+
+
+//            MongoIterable<String> strings = mongoClient.listDatabaseNames();
+//            MongoCursor<String> cursor = strings.cursor();
+//            while (cursor.hasNext()){
+//                System.out.println(cursor.hasNext());
+//            }
+        }
     }
 }
+
+
